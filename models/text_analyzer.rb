15befee9a03bb058dc635d072a3342806c85c5f1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-18 17:19:57 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -11,11 +11,11 @@ class TextAnalyzer
   end
 
   def count_of_vowels
-    text.scan(/[aeiou]).count
+    text.scan(/[aeiou]/).count
   end
 
   def count_of_consonants
-    text.scan(/[bcdfghjklmnpqrstvwxyz]).count
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
   def most_used_letter
